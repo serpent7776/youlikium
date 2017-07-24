fcc6f5a5271a1bafeb3f78a487bba9d4fc03cb38
serpent7776 <serpent7776@gmail.com>
2017-07-24 21:29:21 +0200

    initial commit

---
+++ lib/youlikium.rb
@@ -0,0 +1,44 @@
+require 'selenium-webdriver'
+
+class Youlikium
+
+	def initialize
+		profile = Selenium::WebDriver::Firefox::Profile.new
+		# disable loading images
+		profile['permissions.default.image'] = 2
+		$driver = Selenium::WebDriver.for :firefox, profile: profile
+		$driver.navigate.to 'https://youtube.com'
+	end
+
+	def is_logged_in
+		begin
+			$driver.find_element(id: 'yt-masthead-account-picker')
+			return true
+		rescue Selenium::WebDriver::Error::NoSuchElementError
+			return false
+		end
+	end
+
+	def get_id(line)
+		# try matching youtube-dl file name
+		m = %r{-([-_0-9a-zA-Z]{11})\.\w+}.match line
+		if m then
+			return m[1]
+		end
+		# try matching url
+		m = %r{\byoutube\.com/watch\?v=([-_0-9a-zA-Z]{11}\b)}.match line
+		if m then
+			return m[1]
+		end
+		raise ArgumentError.new "Could not extract video if from '#{line}'"
+	end
+
+	def open_video(id)
+		$driver.navigate.to "https://www.youtube.com/watch?v=#{id}"
+	end
+
+	def like_current
+		$driver.find_element(class: 'like-button-renderer-like-button-unclicked').click
+	end
+
+end
